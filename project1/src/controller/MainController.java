fbeb706a731b9824fc39106ece109cd5a4d8c589
royeleke <70130073+royeleke@users.noreply.github.com>
2021-12-03 15:47:20 -0500

    Add files via upload

---
+++ project1/src/controller/MainController.java
@@ -0,0 +1,12 @@
+package controller;
+
+import infrastructureLayer.MainService;
+
+public class MainController {
+	private static MainService mainService;
+	
+	public static void main(String[] args) {
+			mainService= new MainService();
+			System.out.println(mainService.listMaintRequests());
+	}
+}
